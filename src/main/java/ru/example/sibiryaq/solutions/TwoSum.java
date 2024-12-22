c73b5c35e846b22e49dec7f973ab533838d424d6
Sibiryaq <4totakoeaaa@mail.ru>
2024-12-23 01:42:41 +0700

    Two Sum

---
+++ src/main/java/ru/example/sibiryaq/solutions/TwoSum.java
@@ -0,0 +1,17 @@
+package ru.example.sibiryaq.solutions;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; i++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+
+}
