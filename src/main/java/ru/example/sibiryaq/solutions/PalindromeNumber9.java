695acae8f85b2e94a2cc6ed2bb23bcb8fecb2f06
Sibiryaq <4totakoeaaa@mail.ru>
2024-12-23 01:48:59 +0700

    PalindromeNumber

---
+++ src/main/java/ru/example/sibiryaq/solutions/PalindromeNumber9.java
@@ -0,0 +1,10 @@
+package ru.example.sibiryaq.solutions;
+
+public class PalindromeNumber9 {
+    public boolean isPalindrome(int x) {
+        String number = String.valueOf(x);
+        String reverse = new StringBuilder(number).reverse().toString();
+
+        return number.equals(reverse);
+    }
+}
--- src/main/java/ru/example/sibiryaq/solutions/TwoSum1.java
+++ src/main/java/ru/example/sibiryaq/solutions/TwoSum1.java
@@ -1,6 +1,6 @@
 package ru.example.sibiryaq.solutions;
 
-public class TwoSum {
+public class TwoSum1 {
 
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
