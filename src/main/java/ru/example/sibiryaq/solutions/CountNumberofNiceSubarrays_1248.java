e371bb778e34221c2f09079fc3a2fb766530b9f2
Sibiryaq <4totakoeaaa@mail.ru>
2025-02-01 21:37:17 +0700

    1248 task

---
+++ src/main/java/ru/example/sibiryaq/solutions/CountNumberofNiceSubarrays_1248.java
@@ -0,0 +1,21 @@
+package ru.example.sibiryaq.solutions;
+
+public class CountNumberofNiceSubarrays_1248 {
+
+
+
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int[] cnt = new int[n + 1];
+        cnt[0] = 1;
+        int ans = 0, t = 0;
+        for (int v : nums) {
+            t += v & 1;
+            if (t - k >= 0) {
+                ans += cnt[t - k];
+            }
+            cnt[t]++;
+        }
+        return ans;
+    }
+}
