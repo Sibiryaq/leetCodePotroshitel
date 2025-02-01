2de2b6e4613df109eabf1918e3c46cdbd72c2c39
Sibiryaq <4totakoeaaa@mail.ru>
2025-02-01 15:01:40 +0700

    438 task

--- src/main/java/ru/example/sibiryaq/solutions/FindAllAnagrams_438.java
+++ src/main/java/ru/example/sibiryaq/solutions/FindAllAnagrams_438.java
@@ -1,4 +1,32 @@
 package ru.example.sibiryaq.solutions;
 
+import java.util.*;
+
 public class FindAllAnagrams_438 {
+    public static void main(String[] args) {
+        findAnagrams("cbaebabacd", "abc");
+        findAnagrams("", "");
+    }
+
+
+
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        char[] sc = s.toCharArray();
+        char[] pc = p.toCharArray();
+
+        List<Integer> res = new ArrayList<>();
+        Arrays.sort(pc);
+
+        int k = pc.length;
+        for (int i = 0; i <= sc.length - k; i++) {
+            String str = s.substring(i, i + k);
+            char[] sort = str.toCharArray();
+            Arrays.sort(sort);
+            if (Arrays.equals(pc, sort)) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
 }
