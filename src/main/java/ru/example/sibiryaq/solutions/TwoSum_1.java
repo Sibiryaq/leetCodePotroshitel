2741f7c458bd252f7f0595a26b4c6f72c8653cb1
Sibiryaq <4totakoeaaa@mail.ru>
2025-05-04 20:07:56 +0700

    TwoSum_1

--- src/main/java/ru/example/sibiryaq/solutions/TwoSum_1.java
+++ src/main/java/ru/example/sibiryaq/solutions/TwoSum_1.java
@@ -1,6 +1,6 @@
 package ru.example.sibiryaq.solutions;
 
-public class TwoSum1 {
+public class TwoSum_1 {
 
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
